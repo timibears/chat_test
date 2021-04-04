a8df839b4cd20870457756a989e871e3acf2b258
Kelp <kelp@ljit.io>
2021-04-04 15:54:49 +0800

    add an http api to get messages

--- src/backend/routers/web-router.js
+++ src/backend/routers/web-router.js
@@ -1,8 +1,10 @@
 const ExpressRouter = require('../models/express-router');
 const baseHandler = require('../handlers/base-handler');
+const messageHandler = require('../handlers/message-handler');
 
 const webRouter = new ExpressRouter();
 
 webRouter.get('/', baseHandler.getBaseView);
+webRouter.get('/messages', messageHandler.getMessages);
 
 module.exports = webRouter.router;
